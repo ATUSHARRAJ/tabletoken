23af0ce519558c2421c6bfe52a9e99f93332197f
Tushar Raj <153943708+ATUSHARRAJ@users.noreply.github.com>
2026-05-16 17:09:42 +0530

    Enable secure email transport

--- server/config/email.js
+++ server/config/email.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host:   process.env.EMAIL_HOST,
   port:   process.env.EMAIL_PORT,
-  secure: false,
+  secure: true,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
