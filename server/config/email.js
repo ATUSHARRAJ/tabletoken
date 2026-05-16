5682fcb0cf7a3e0e13a14e451f9370a52afe45be
Tushar Raj <153943708+ATUSHARRAJ@users.noreply.github.com>
2026-05-16 17:28:47 +0530

    Update email transport configuration settings

--- server/config/email.js
+++ server/config/email.js
@@ -2,9 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host:   process.env.EMAIL_HOST,
-  port:   process.env.EMAIL_PORT,
-  secure: true,
-  family: 4,
+  port:   parseInt(process.env.EMAIL_PORT),
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
